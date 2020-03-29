4fba033827b00e043fe37aa5e478b3854552dc10
Klaus Crusius <klaus.crusius@web.de>
2020-03-29 23:25:39 +0200

    access external packages

--- src/PkgVersion.jl
+++ src/PkgVersion.jl
@@ -1,21 +1,41 @@
 module PkgVersion
+using Pkg
 
-function project_data(__source__, name, T::Type, default)
-    _name = T(default)
-    __source__.file === nothing && return _name
-    _file = abspath(dirname(dirname(String(__source__.file))), "Project.toml")
-    isfile(_file) && open(_file) do io
-        while !eof(io)
-            _line = readline(io)
-            if startswith(_line, string(name, ' ', '='))
-                _name = T(split(_line, '"')[2])
-            end
-        end
+function project_data(m::Module, name, T, default)
+    sname = Symbol(name)
+    if isconst(m, sname)
+        T(getfield(m, sname))
+    else
+        pf = pkgdir(m)
+        pf === nothing && return T(default)
+        pf = Pkg.Types.projectfile_path(pkgdir(m))
+        project_data(pf, name, T, default)
     end
-    _name
 end
-macro Version(default=0); project_data(__source__, "version", VersionNumber, default); end
-macro Uuid(default=0); project_data(__source__, "uuid", Base.UUID, default); end
-macro Author(default=0); project_data(__source__, "authors", String, default); end
+function project_data(__source__::LineNumberNode, name, T, default)
+    __source__.file === nothing && return T(default)
+    pf = abspath(dirname(dirname(String(__source__.file))), Base.project_names[end])
+    project_data(pf, name, T, default)
+end    
+function project_data(pf::AbstractString, name, T::Union{Type,Function}, default)
+    pf === nothing && return T(default)
+    pr = Pkg.Operations.read_project(pf)
+    if  Symbol(name) ∈ fieldnames(typeof(pr))
+        res = getfield(pr, Symbol(name))
+        res === nothing ? T(default) : res
+    else
+        res = get(()->T(default), pr.other, string(name))
+        res isa AbstractVector ? res[1] : res
+    end
+end
+macro Version(default=0); project_data(__source__, :version, VersionNumber, default); end
+macro Uuid(default=0); project_data(__source__, :uuid, Base.UUID, default); end
+macro Author(default="unknown"); project_data(__source__, "authors", string, default); end
+
+Version(m::Module, default=0) = project_data(m, :version, VersionNumber, default)
+Uuid(m::Module, default=0) = project_data(m, :uuid, Base.UUID, default)
+Author(m::Module, default="unknown") = project_data(m, "authors", string, default)
+
+const VERSION = @PkgVersion.Version
 
 end # module
