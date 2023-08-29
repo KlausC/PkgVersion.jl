59e2d8e90cb67b229bec878157269cdd2248c691
KlausC <klausC@users.noreply.github.com>
2023-08-29 10:59:43 +0200

    compatible with v1.0

--- src/PkgVersion.jl
+++ src/PkgVersion.jl
@@ -3,7 +3,7 @@ using Pkg
 
 function project_data(m::Module, name, T, default)
     function _pkgdir(m::Module)
-        if Base.VERSION < v"1.4"
+        @static if Base.VERSION < v"1.4"
             pf = Base.pathof(Base.moduleroot(m))
             pf === nothing ? nothing : abspath(pf, "..", "..")
         else
@@ -15,17 +15,24 @@ function project_data(m::Module, name, T, default)
     pf = Pkg.Types.projectfile_path(pf)
     project_data(pf, name, T, default)
 end
+
+function project_variable(pr::Dict, name, T, default)
+    res = get(() -> T(default), pr, string(name))
+    res = res isa AbstractVector ? res[1] : res
+    T isa Type ? T(res) : res
+end
+function project_variable(pr, name, T, default)
+    sname = Symbol(name)
+    res = isdefined(pr, sname) ? getfield(pr, sname) : nothing
+    res === nothing ? project_variable(pr.other, name, T, default) : res
+end
+
 function project_data(pf::AbstractString, name, T::Union{Type,Function}, default)
     pf === nothing && return T(default)
     pr = Pkg.Operations.read_project(pf)
-    if Symbol(name) ∈ fieldnames(typeof(pr))
-        res = getfield(pr, Symbol(name))
-        res === nothing ? T(default) : res
-    else
-        res = get(() -> T(default), pr.other, string(name))
-        res isa AbstractVector ? res[1] : res
-    end
+    project_variable(pr, name, T, default)
 end
+
 macro Version(default=0)
     project_data(__module__, :version, VersionNumber, default)
 end
